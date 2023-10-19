796f1df6ffb04fce2a210ad8842d6a64c4e7101c
RSSH7275 <ramashreevalunj@gmail.com>
2023-10-19 12:26:46 +0530

    project commit-2

--- src/Components/CardDisplay.jsx
+++ src/Components/CardDisplay.jsx
@@ -10,10 +10,10 @@ import {
   GlobalOutlined,
   HeartFilled,
 } from "@ant-design/icons";
-import { Avatar, Card, Modal, Input,Divider } from "antd";
+import { Avatar, Card, Modal, Input, Divider } from "antd";
 import { EditTwoTone } from "@ant-design/icons";
 import ModalContent from "./ModalContent";
-import {updateUser,deleteUser} from "../UserSlice/UserSlice";
+import { updateUser, deleteUser } from "../UserSlice/UserSlice";
 
 const { Meta } = Card;
 
@@ -22,7 +22,7 @@ const { Meta } = Card;
 function CardDisplay(item) {
   const { id, name, username, phone, email, website } = item.item;
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isHeart,setHeart]=useState(false)
+  const [isHeart, setHeart] = useState(false);
 
   const formRef = useRef(null);
   const dispatch = useDispatch();
@@ -39,65 +39,68 @@ function CardDisplay(item) {
     formRef.current.submit();
     setIsModalOpen(false);
   };
-  
+
   const onFinish = (values) => {
     // console.log("Success:", values);
-    dispatch(updateUser({
-      id:id,
-      name:values.name,
-      email:values.email,
-      phone:values.phone,
-      website:values.Website,
-    }))
-   
+    dispatch(
+      updateUser({
+        id: id,
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+        website: values.Website,
+      })
+    );
   };
   const onFinishFailed = (errorInfo) => {
-    
     const count = Object.keys(errorInfo.values).length;
-    let obj={};
-      
-    for(let i=0;i<count;i++){
-     if((errorInfo.values.name!==undefined) && (errorInfo.values.name.length)){
-       obj.name=errorInfo.values.name;
+    let obj = {};
+
+    for (let i = 0; i < count; i++) {
+      if (errorInfo.values.name !== undefined && errorInfo.values.name.length) {
+        obj.name = errorInfo.values.name;
       }
-      if((errorInfo.values.email!==undefined)&&(errorInfo.values.email.length)){
+      if (
+        errorInfo.values.email !== undefined &&
+        errorInfo.values.email.length
+      ) {
         let emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-        
-        if(emailReg.test(errorInfo.values.email)===true){
-          obj.email=errorInfo.values.email
-        }
-        else{
+
+        if (emailReg.test(errorInfo.values.email) === true) {
+          obj.email = errorInfo.values.email;
+        } else {
           continue;
         }
-     }
-     if((errorInfo.values.phone!==undefined)&&(errorInfo.values.phone.length)){
-       obj.phone=errorInfo.values.phone;
-     }
-     if((errorInfo.values.Website!==undefined)&&(errorInfo.values.Website.length)){
-       obj.Website=errorInfo.values.Website;
-     }
-
-
+      }
+      if (
+        errorInfo.values.phone !== undefined &&
+        errorInfo.values.phone.length
+      ) {
+        obj.phone = errorInfo.values.phone;
+      }
+      if (
+        errorInfo.values.Website !== undefined &&
+        errorInfo.values.Website.length
+      ) {
+        obj.website = errorInfo.values.Website;
+      }
     }
 
-    let isEmpty=Object.keys(obj).length
-    
+    let isEmpty = Object.keys(obj).length;
 
-    if(isEmpty!==0){
-      obj.id=id;
-     dispatch(updateUser(obj));
+    if (isEmpty !== 0) {
+      obj.id = id;
+      dispatch(updateUser(obj));
     }
-
   };
 
-  const handleDelete=(e)=>{
-   dispatch(deleteUser({id:id}));
-  }
+  const handleDelete = (e) => {
+    dispatch(deleteUser({ id: id }));
+  };
 
-  const handleHeart=(e)=>{
-    setHeart(prevState=> !prevState)
-  }
- 
+  const handleHeart = (e) => {
+    setHeart((prevState) => !prevState);
+  };
 
   return (
     <>
@@ -109,10 +112,20 @@ function CardDisplay(item) {
             src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`}
           />
         }
-        
         actions={[
-          isHeart?(<HeartFilled onClick={handleHeart}  style={{color:"red"}}  key="favourite1"/>):
-          (<HeartOutlined onClick={handleHeart}  style={{color:"red"}}  key="favourite" />),
+          isHeart ? (
+            <HeartFilled
+              onClick={handleHeart}
+              style={{ color: "red" }}
+              key="favourite1"
+            />
+          ) : (
+            <HeartOutlined
+              onClick={handleHeart}
+              style={{ color: "red" }}
+              key="favourite"
+            />
+          ),
           <EditOutlined onClick={editModal} key="edit" />,
           <DeleteFilled onClick={handleDelete} key="delete" />,
         ]}
@@ -136,9 +149,14 @@ function CardDisplay(item) {
           onOk={handleOk}
           onCancel={handleCancel}
         >
-          <Divider/>
-          <ModalContent ref={formRef} onFinish={onFinish} onFinishFailed={onFinishFailed} id={id}/>
-          <Divider/>
+          <Divider />
+          <ModalContent
+            ref={formRef}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            id={id}
+          />
+          <Divider />
         </Modal>
       </Card>
     </>
@@ -146,4 +164,3 @@ function CardDisplay(item) {
 }
 
 export default CardDisplay;
-
